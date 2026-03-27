eeaff2ae2740a6c2924fa5339413d4b551a7b6b9
Wehner.Johann <wehner.johann2425@schule.fosbos-bayreuth.de>
2026-03-27 08:34:10 +0100

    initial game mechanics

--- AlphaRunner.java
+++ AlphaRunner.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 public class AlphaRunner extends SPIEL
 {
     FIGUR Welt, Alpha;
+    
+    public Dinospiel(){
 
+        verschiebeKamera(-40,0);
+    }
 
+    @Override
+    public void bildAktualisierungReagieren(double sekunden){
+        if (Alpha.nenneAktivenZustand() != "tot"){
+            Alpha.verschiebenUm (4*sekunden,0);
+            if (Alpha.nenneMx() >= 30){
+                Alpha.verschiebeKamera(4*sekunden,0);
+            }
+        }
+    }
 }
