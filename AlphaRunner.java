98f95099b60e108bbd324a31c9f4d92252be8c89
Eulefeld.Marc <eulefeld.marc2425@schule.fosbos-bayreuth.de>
2026-03-27 08:38:54 +0100

    Konstruktor

--- AlphaRunner.java
+++ AlphaRunner.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class AlphaRunner extends SPIEL
 {
     FIGUR Welt, Alpha;
+    TEXT text; 
 
-
+public AlphaRunner {
+    Welt = new FIGUR();
+    Welt = 
+    Alpha = new FIGUR();
+}
 }
