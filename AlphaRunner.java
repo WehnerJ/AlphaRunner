3da5c3c65ff448f0bf4e454bf501ad14622de11d
Wehner.Johann <wehner.johann2425@schule.fosbos-bayreuth.de>
2026-04-24 08:18:50 +0200

    fixes

--- AlphaRunner.java
+++ AlphaRunner.java
@@ -7,51 +7,42 @@ public class AlphaRunner extends SPIEL
 
     public AlphaRunner(){
         //Hintergrund
-        setzeHintergrundgrafik(wueste20000.png);
+        setzeHintergrundgrafik("wueste20000.png");
 
         //Welt 
-        Welt = new FIGUR(boden20000.png);
+        Welt = new FIGUR("boden20000.png");
 
         Welt.verschiebenUm(0, 0);
         Welt.macheStatisch();
 
         //Runner
-        Alpha = new FIGUR(gurke.png);
-        Alpha.setzeMittelpunkt();
-        Alpha.fuegeZustandVonEinzelbildernHinzu(Powerup, powerup1.png);
+        Alpha = new FIGUR("gurke.png");
+        Alpha.setzeMittelpunkt(1, 1);
+        Alpha.fuegeZustandVonEinzelbildernHinzu("Powerup", "powerup1.png");
         Alpha.macheDynamisch();
 
         //Powerup
-  
+
         //Gegner
     }
 
     //Laufen
-    @Override
-    public void bildAktualisierungReagieren(double sekunden){
-        if (Alpha.nenneAktivenZustand() != "tot"){
-            Alpha = new FIGUR();
-            Alpha.setzeMittelpunkt();
-            Alpha.fuegeZustandVonEinzelbildernHinzu();
-            Alpha.macheDynamisch();
-
-            //Powerup
-
-            //Gegner
-        }
+    
 
         @Override
         public void bildAktualisierungReagieren(double sekunden){
-        if (DTaste == 68){
+        if (Alpha.nenneAktivenZustand() != "tot"){
+        }
+        else if (DTaste == 68){
             Alpha.verschiebenUm (4*sekunden,0);
             if (Alpha.nenneMx() >= 30){
-                Alpha.verschiebeKamera(4*sekunden,0);
+                verschiebeKamera(4*sekunden, 0);
             }
         }
         else if (DTaste == 65){
             Alpha.verschiebenUm (-4*sekunden,0);
             if (Alpha.nenneMx() >= 30){
-                Alpha.verschiebeKamera(-4*sekunden,0);
+                verschiebeKamera(-4*sekunden,0);
             }
         }
     }
@@ -64,9 +55,9 @@ public class AlphaRunner extends SPIEL
         else if (taste == TASTE.A){
             DTaste = taste;
         }
-        if (taste == TASTE.LEER){
+        else if (taste == TASTE.LEER){
             DTaste = taste;
-            Alpha.springe(0, 10);
+            Alpha.springe(10);
         }
     }
 }
